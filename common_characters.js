dde77fec431847ee5ec5abeaa13a0c21615e143b
Christopher Hullman <christopherhullman@gmail.com>
2022-07-19 09:22:39 +1000

    common characters morning chal complete

---
+++ common_characters.js
@@ -0,0 +1,34 @@
+// Common Characters
+// Write code which will return the common characters found in
+// two different strings. 
+
+// Note:
+
+// You should only need to return the characters once.
+
+// Spaces should not be included
+
+// Ignore letter case
+
+// The order of characters in the resulting string should follow the order of characters in string1
+
+
+// Examples:
+
+// commonCharacters("efg", "feg") => "efg"
+
+// commonCharacters("We are both dragon energy", "I am God's vessel") => "eaodg"
+
+// commonCharacters("Alright alright alright", "") => ""
+
+function commonCharacters(string1, string2) {
+    result = ""
+    string1.toLowerCase().split('').forEach(function (c) {
+       if (c !== " " && string2.toLowerCase().includes(c) && !(result.includes(c))) {
+          result += c
+       }    
+    });
+    return result;
+ }
+ 
+ module.exports = {commonCharacters}
